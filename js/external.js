359e5d9e9d0356effe36096760e7aeb394529357
Amanda Garrison Woods <you@example.com>
2020-07-25 21:46:47 -0500

    javascript with html exercises

---
+++ js/external.js
@@ -0,0 +1,27 @@
+"use strict";
+
+console.log("Hello from external JavaScript")
+alert("Welcome to my Website!");
+//////////////////////////////
+var color = prompt("What is your favorite color?");
+console.log("The user entered: " + color);
+alert("Great, " + color + " is my favorite color too!");
+//////////////////////////
+var pricePerDay = 3;
+var mermaid = 3;
+var bear = 5;
+var herc = 1;
+
+var mermaid = prompt("How many days would you like to watch Little Mermaid?")
+console.log("Little Mermaid for " + mermaid + " days.");
+var bear = prompt("How many days would you like to watch Brother Bear?")
+console.log("Brother Bear for " + bear + " days.");
+var herc = prompt("How many days would you like to watch Hercules?")
+console.log("Hercules for " + herc + " days.");
+
+
+
+
+/////////////////////////////
+var google = prompt("How much do you make at Google?")
+var googleHours = prompt("How many hours did you work this week?")
