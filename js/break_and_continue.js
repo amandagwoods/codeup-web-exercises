247dc9f649c0ae7efc0f5757ab6c8cca5727b4b3
Amanda Garrison Woods <you@example.com>
2020-07-30 17:03:38 -0500

    while loop exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,14 +1,19 @@
 "use strict";
 
-var random = Math.floor((Math.random()*50)+1);
-
-console.log("Random odd number to skip is: " + random);
+var input = prompt("Enter an odd number")
+console.log("Your odd number to skip is: " + input);
+    // if (input = ""){
+    //     prompt("Enter an odd number");
+    // }
+    // if(input % 2 !== 0){
+    //     prompt("Try an odd number");
+    // }
 
 for (var i = 1; i < 50; i++) {
     if (i % 2 === 0) {
         continue;
     }
-    if (random === i) {
+    if (input == i) {
         console.log("Yikes! Skipping number " + i);
     } else {
         console.log("Here is an odd number: " + i);
--- js/for_loops.js
+++ js/for_loops.js
@@ -1,9 +1,18 @@
+"use strict";
+
 //For Loops
 //2
 // for (var i = 0; i < 10; i++) {
 //     console.log('for loop iteration #' + i);
 // }
+function showMultiplicationTable(aNumber){
+    for(var c = 1; c <= 10; c++){
+        console.log(aNumber+" x " + c + " = " + (aNumber * c));
 
+    }
+
+}
+showMultiplicationTable(7)
 
 //3
 function randomEvenOdd() {
@@ -16,7 +25,16 @@ function randomEvenOdd() {
         }
     }
 }
-randomEvenOdd()
+
+//fer's function
+// function randomEvenOdd (var i = 1; i < 10; i++) {
+//     if (getRandomInt(20,200) % 2 === 0>) {
+//     console.log("even");
+//     }else {
+//     console.log("odd");
+// }
+// }
+// randomEvenOdd()
 
 
 //4
@@ -30,6 +48,21 @@ function numberPyramid() {
     }
 }
 numberPyramid();
+
+
+//fer's smallest function
+for (var z = 1; z <= 9; z++){
+    console.log(z.toString().repeat(z));
+}
+//long version
+// for (var outer = 1; outer <= 9; outer++){
+//     var result = "";
+//     console.log("outer", outer);
+//     for (var inner = 1; inner <= outer ; inner++){
+//         result += outer;
+//     }
+//     console.log(outer);
+// }
 
 //5
 for (var i = 100; i > 5; i = i - 5){
