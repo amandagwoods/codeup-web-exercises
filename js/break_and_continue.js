4faeab5c70b7145be99549a78caf114724a04be3
Amanda Garrison Woods <you@example.com>
2020-07-31 16:40:12 -0500

    updates to break/continue & while

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,7 +1,7 @@
 "use strict";
 
-var input = prompt("Enter an odd number")
-console.log("Your odd number to skip is: " + input);
+// var input = parseInt(prompt("Enter an odd number between 1 and 50."));
+// console.log("Your odd number to skip is: " + input);
     // if (input = ""){
     //     prompt("Enter an odd number");
     // }
@@ -9,11 +9,21 @@ console.log("Your odd number to skip is: " + input);
     //     prompt("Try an odd number");
     // }
 
+//Walkthrough
+var input;
+
+while(true){
+    input = parseInt(prompt("Enter an odd number between 1 and 50."));
+    if (input % 2 !== 0 && input > 0 && input < 51)
+    break;
+    console.log("Your odd number to skip is: " + input);
+}
+//////
 for (var i = 1; i < 50; i++) {
     if (i % 2 === 0) {
         continue;
     }
-    if (input == i) {
+    if (input === i) {
         console.log("Yikes! Skipping number " + i);
     } else {
         console.log("Here is an odd number: " + i);
--- js/while.js
+++ js/while.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 
 var i = 1;
 while(i < 65536) {
@@ -6,28 +6,34 @@ while(i < 65536) {
     console.log(i);
 
 }
+
 /////////
 // This is how you get a random number between 50 and 100
 // var allCones = Math.floor(Math.random() * 50) + 50;
 // // This expression will generate a random number between 1 and 5
 // Math.floor(Math.random() * 5) + 1;
-
-5 cones sold...  // if there are enough cones
-Cannot sell you 6 cones I only have 3...  // If there are not enough cones
-    Yay! I sold them all! // If there are no more cones
+// 5 cones sold...  // if there are enough cones
+// Cannot sell you 6 cones I only have 3...  // If there are not enough cones
+//     Yay! I sold them all! // If there are no more cones
 
 // do {
 //     //body
 // }while(/*condition*/);
 
-
+//
 var allCones = Math.floor(Math.random() * 50) + 50;
-Math.floor(Math.random() * 5) + 1;
-
-
 do {
-    console.log(allCones);
-    i++;
-} while (i >= allCones){
+    var cones = Math.floor(Math.random() * 5) + 1;
+    //decide if we have enough
+    if(cones <= allCones){
+        //sell some cones
+        console.log(cones + " cones sold...");
+        allCones -= cones;
+    } else {
+        console.log("Cannot sell you " + cones + " I only have " + allCones );
+    }
 
-}
+}while (allCones > 0);
+
+console.log("Yay I sold them all!");
+
