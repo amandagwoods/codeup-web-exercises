96b9cd6d543aa2cb56730d5d63fe02df748c4059
Amanda Garrison Woods <you@example.com>
2020-08-03 09:30:05 -0500

    mannipulating arrays lesson

---
+++ js/manipulating_arrays_practice.js
@@ -0,0 +1,17 @@
+var daysOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday'];
+console.log(daysOfTheWeek);
+
+daysOfTheWeek.push("Friday");
+daysOfTheWeek.push("Saturday");
+
+daysOfTheWeek.unshift("Sunday");
+
+console.log(daysOfTheWeek);
+
+daysOfTheWeek.push("true");
+
+daysOfTheWeek.pop();
+console.log(daysOfTheWeek);
+
+daysOfTheWeek.shift();
+console.log(daysOfTheWeek);
