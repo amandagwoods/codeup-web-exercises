ec93c5d79ca568ecbf9f34929532468049c01914
Amanda Woods <amanda.garrison.woods@gmail.com>
2020-09-17 11:54:51 -0500

    struggling through weather

--- js/weather_map.js
+++ js/weather_map.js
@@ -10,8 +10,9 @@ $(document).ready(function(){
         center: {lng: -97.3208, lat: 32.7254},
     });
 
+
 //-----------marker----------
-    var marker = new mapboxToken.Marker();
+    var marker = new mapboxToken.marker();
     marker.settingLngLat({lng: -97.3208, lat: 32.7254});
     marker.addTo(map);
     marker.setDraggable(true);
@@ -32,47 +33,56 @@ $(document).ready(function(){
 
     //----------5 day forecast---------------
 
-    $.ajax("").done(function(fiveDay){
-        var html = "";
-        $("#weather").empty();
-        fiveDay.forEach(function(weather){
-            html = "<div class="card">";
-            html += "<img src=\"https://via.placeholder.com/350x150\" class=\"card-img-top\" alt\="...">\"
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
-                    </div>
-            </div>
+    function weeklyForecast() {
+        $.get(weatherUrl, weatherOptions).done(function (data) {
+            console.log(data);
+            console.log(data.daily);
+            console.log(data.daily[0]);
+            data.daily.forEach(function (day, index) {
+                if (index <= 4) {
+                    var grabDate = new Date(day.dt * 1000);
+                    var html = "<div class='card-body'>";
+                    html += "<div class='card-header text-center'>" + " " + (grabDate.getMonth() + 1) + "/" + grabDate.getDate() + "/" + grabDate.getFullYear() + "</div>";
+                    html += "<li class='list-group-item'>" + "High/Lows: " + day.temp.max + " &#8457;" + "/" + day.temp.min + " &#8457;" + "</li>";
+                    html += "<li class='list-group-item'>" + "Description: " + day.weather[0].description + "</li>";
+                    html += "<li class='list-group-item'>" + "Humidity: " + day.humidity + "</li>";
+                    html += "<li class='list-group-item'>" + "Wind: " + day.wind_speed + "</li>";
+                    html += "<li class='list-group-item'>" + "Pressure: " + day.pressure + "</li>";
+                    html += "</div>";
+                    $('#weather-info').append(html);
+                }
+            });
+        });
+    };
 
-        })
-    })
+    //---------weather based on marker-------
+    function markerDaily() {
+        var lngLat = marker.getLngLat();
+        var longitude = lngLat.lng;
+        var latitude = lngLat.lat;
+        weatherOptions.lat = latitude
+        weatherOptions.lon = longitude
+        reverseGeocode({lng: longitude, lat: latitude}, mapboxKey).then(function (result) {
+            $('#current-city').empty()
+            $('#current-city').append("Current Location " + result)
+        });
+        weeklyForecast();
+    }
 
 
-    // $.ajax("").done(function(fiveDay){
-    //     var html = "";
-    //     $("#weather").empty();
-    //     fiveDay.forEach(function(weather) {
-    //         html = "<div class=\"card\">\n" +
-    //             "    <img class=\"card-img-top\" src=\"https://via.placeholder.com/350x150\" alt=\"Card image cap\">\n" +
-    //             "    <div class=\"card-body\">\n" +
-    //             "      <h5 class=\"card-title\">"+weather.list.i[1]+"</h5>\n" +
-    //             "      <p class=\"card-text\">"+weather.content+"</p>\n" +
-    //             "      <p class=\"card-text\"><small class=\"text-muted\">"+weather.date+"</small></p>\n" +
-    //             "      <p class=\"card-text\"><small class=\"text-muted\">"+weather.categories+"</small></p>"+
-    //             "      <p class=\"card-footer\"><small class=\"text-muted\">"+weather.categories+"</small></p>"+
-    //             "    </div>\n" +
-    //             "  </div>";
-    //         $("#posts").append(html);
-    //                 <div class="card-footer">
-    //                     <small class="text-muted">Last updated 3 mins ago</small>
-    //                 </div>
-    //         </div>
-    //     });
-    // });
-
+    //----------search bar-------------
+    var searchBar = document.querySelector('#input');
+    $("newLocation").click(function (e) {
+        e.preventDefault();
+        geocode(searchBar.value, mapboxKey).then(function (result) {
+            map.setCenter(result);
+            map.setZoom(10);
+            marker.setLngLat(result);
+            markerDaily();
+        });
+    });
+    weeklyForecast();
+    marker.on("dragend", markerDaily());
 
 
 
