a900df012aca4b72bb6339c9c712c6bad40505e4
Amanda Woods <amanda.garrison.woods@gmail.com>
2020-09-18 17:06:52 -0500

    completed exercise

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -55,6 +55,35 @@ let exerienceUsers = users.reduce((total,user) =>{
     console.log(exerienceUsers)
 
 // Use .reduce to get the longest email from the list of users.
+let longEmail = users.reduce ((total,user) =>{
+    if (total.length < user.email.length){
+        return user.email
+    }
+    return total;
+
+},"");
+    console.log(longEmail)
 
 //Use .reduce to get the list of user's names in a single string. Example: Your
 // instructors are: ryan, luis, zach, fernando, justin.
+let names = users.reduce ((sentence,user) =>
+    sentence === ''? user.name : sentence+', '+user.name,"Your instructors are: " );
+   console.log(names+ ".");
+//     if (total === ""){
+//         return total + "Your instructors are: " + user.name;
+//     }
+//     return total + ", " + user.name;
+// });
+// console.log(yourInstructor)
+
+//Bonus** Use .reduce to get the unique list of languages from the list of users.
+let uniqueLangs = users.reduce((langList, user) => {
+    for (let lang of user.languages){
+        if (!langList.includes(lang)) {
+            langList.push(lang);
+        }
+    }
+    return langList
+}, []);
+
+console.log(uniqueLangs);
